60e9abdca2f769f0dca768d93366c54cc0f929a3
shubham-mhatre <mhatreshubham03@gmail.com>
2026-01-25 16:40:09 +0530

    quiz app backend initial setup

---
+++ quiz-app-backend/src/test/java/sm/quiz/QuizAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sm.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
