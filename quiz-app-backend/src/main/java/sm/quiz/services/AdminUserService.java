cbcea26b0ef45d2e7e7fcf903210b3687ae6381a
shubham-mhatre <mhatreshubham03@gmail.com>
2026-03-16 23:10:01 +0530

    admin user created at field sent in response

--- quiz-app-backend/src/main/java/sm/quiz/services/AdminUserService.java
+++ quiz-app-backend/src/main/java/sm/quiz/services/AdminUserService.java
@@ -38,6 +38,7 @@ public class AdminUserService {
         savedResponse.setUserId(saved.getUserId());
         savedResponse.setUsername(saved.getUsername());
         savedResponse.setRole(saved.getRole());
+        savedResponse.setCreatedAt(saved.getCreatedAt());
         
         return savedResponse;
     }
